e940da9b428da39cf5c4482403c7727a0a11432a
Barry van Oudtshoorn <bvanoudtshoorn@gmail.com>
2011-12-17 02:37:35 +0000

    Updating Performer.Base

--- Source/performer.Base.js
+++ Source/performer.Base.js
@@ -10,6 +10,8 @@ barryvan.tp.performer.Base = new Class({
 	options: {
 		filterHigh: 200,
 		filterLow: 1,
+		velocityHigh: 64,
+		velocityLow: 0,
 		filterMode: 'pass'
 	},
 	
@@ -21,13 +23,21 @@ barryvan.tp.performer.Base = new Class({
 		this.setOptions(options);
 	},
 	
-	filterNote: function(note) {
-		//console.log('filterNote', note, this.options.filterLow, this.options.filterHigh, this.options.filterMode);
+	filterNote: function(data) {
+		var note = data.note || data; // Support handing entire data object or just note
+		var volume = data.volume || 64;
+		
+		var allowPitch = false;
+		
 		if (this.options.filterMode === 'cut') { // band cut
-			return (note <= this.options.filterLow && note >= this.options.filterHigh) ? true : false;
+			allowPitch = (note <= this.options.filterLow && note >= this.options.filterHigh);
 		} else { // band pass
-			return (note >= this.options.filterLow && note <= this.options.filterHigh) ? true : false;
+			allowPitch = (note >= this.options.filterLow && note <= this.options.filterHigh);
 		}
+		
+		if (!allowPitch) return false;
+		
+		return (volume >= this.options.velocityLow && volume <= this.options.velocityHigh);
 	}
 	
 	// noteEvent: function(data) { }
